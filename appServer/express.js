c56dd7c0e9d8b9d2b5a008edeb60d5872f8324c3
stiven <sramir70@eafit.edu.co>
2019-01-25 23:15:49 -0500

    Añadiendo express.js modificado

--- appServer/express.js
+++ appServer/express.js
@@ -1,21 +1,62 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var glob = require('glob');
 
-// create express app
-const app = express();
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var compress = require('compression');
+var methodOverride = require('method-override');
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+module.exports = function(app, config) {
+  var env = process.env.NODE_ENV || 'development';
+  app.locals.ENV = env;
+  app.locals.ENV_DEVELOPMENT = env == 'development';
+  
+  app.set('views', config.root + '/app/views');
+  app.set('view engine', 'ejs');
 
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
+  // app.use(favicon(config.root + '/public/img/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(cookieParser());
+  app.use(compress());
+  app.use(express.static(config.root + '/public'));
+  app.use(methodOverride());
 
-// define a simple route
-app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
-});
+  var controllers = glob.sync(config.root + '/app/controllers/*.js');
+  controllers.forEach(function (controller) {
+    require(controller)(app);
+  });
 
-// listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-});
+  app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+  
+  if(app.get('env') === 'development'){
+    app.use(function (err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err,
+        title: 'error'
+      });
+    });
+  }
+
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: {},
+        title: 'error'
+      });
+  });
+
+  return app;
+};
