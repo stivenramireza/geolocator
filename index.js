20772ebd40cd35ba59830eb6bcf9467225c4aafd
stiven <sramir70@eafit.edu.co>
2019-02-02 19:10:59 -0500

    Añadiendo NGINX Web Server

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const config = require('./config')
 
-mongoose.connect(config.db, (err, res) => {
+mongoose.connect(config.db,{ useCreateIndex: true,
+  useNewUrlParser: true } , (err, res) => {
   if (err) {
     return console.log(`Error al conectar a la base de datos: ${err}`)
   }
