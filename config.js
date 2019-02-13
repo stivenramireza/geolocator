fb25a9c138d8e163692ee7841db3c05ba2dc61ac
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-02-12 20:41:24 -0500

    Docker v4.0

--- config.js
+++ config.js
@@ -5,19 +5,19 @@ var path = require('path'),
 var config = {
   development: {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB_URI || 'mongodb://mongo-server:27017/database-geolocator',
-    SECRET_TOKEN: 'tokenGeolocator'
+    db: process.env.MONGODB_URI || 'mongodb://localhost/database-geolocator',
+    SECRET_TOKEN: 'tokenGeolocator' 
   },
 
   test: {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/database-geolocator',
+    db: process.env.MONGODB_URI || 'mongodb://mongo-server/database-geolocator',
     SECRET_TOKEN: 'tokenGeolocator'
   },
 
   production: {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB_URI || 'mongodb://mongo-server:27017/database-geolocator',
+    db: process.env.MONGODB_URI || 'mongodb://mongo-server/database-geolocator',
     SECRET_TOKEN: 'tokenGeolocator'
   }
 };
